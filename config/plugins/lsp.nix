c5971cba5b12b7fe7eba8f6f18b1c85de8d8d5a6
Superredstone <patrickcanal3@gmail.com>
2026-07-31 16:43:07 +0200

    feat(format): add django template formatter

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -1,35 +1,45 @@
 { ... }:
 {
-  plugins.lsp = {
-    enable = true;
-    servers = {
-      clangd.enable = true;
-      nil_ls.enable = true;
-      gopls.enable = true;
-      jdtls.enable = true;
-      lua_ls.enable = true;
-      rust_analyzer = {
-        enable = true;
-        installCargo = false;
-        installRustc = false;
-        installRustfmt = false;
+  plugins = {
+    lsp = {
+      enable = true;
+      servers = {
+        clangd.enable = true;
+        nil_ls.enable = true;
+        gopls.enable = true;
+        jdtls.enable = true;
+        lua_ls.enable = true;
+        rust_analyzer = {
+          enable = true;
+          installCargo = false;
+          installRustc = false;
+          installRustfmt = false;
+        };
+        zls.enable = true;
+        nim_langserver.enable = true;
+        emmet_language_server.enable = true;
+        ts_ls.enable = true;
+        docker_compose_language_service.enable = true;
+        jsonls.enable = true;
+        phpactor.enable = true;
+        qmlls.enable = true;
+        pylsp.enable = true;
+        ruff.enable = true;
       };
-      zls.enable = true;
-      nim_langserver.enable = true;
-      emmet_language_server.enable = true;
-      ts_ls.enable = true;
-      docker_compose_language_service.enable = true;
-      jsonls.enable = true;
-      phpactor.enable = true;
-      qmlls.enable = true;
-      pylsp.enable = true;
-      ruff.enable = true;
+      keymaps = {
+        lspBuf = {
+          "<Space>k" = "hover";
+          "<Space>r" = "rename";
+          "<Space>a" = "code_action";
+        };
+      };
     };
-    keymaps = {
-      lspBuf = {
-        "<Space>k" = "hover";
-        "<Space>r" = "rename";
-        "<Space>a" = "code_action";
+    none-ls = {
+      enable = true;
+      sources = {
+        formatting = {
+          djlint.enable = true;
+        };
       };
     };
   };
